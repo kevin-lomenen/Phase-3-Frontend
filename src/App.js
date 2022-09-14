9b7ac352f8b3fbc96033c2c8725e1023e5ffa9b3
Allan <kipropsallan@gmail.com>
2022-09-14 22:43:35 +0300

    modified

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Home from "./components/pages/Home";
 import DefaultPage from "./components/pages/DefaultPage";
 import Recipes from "./components/pages/Recipes";
 import RecipeDetails from "./components/pages/RecipeDetails";
+import Signup from "./components/layout/Signup";
+import Login from "./components/layout/Login";
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/recipes" component={Recipes} />
           <Route exact path="/recipes/:recipe_id" component={RecipeDetails} />
+          <Route path ="/Signup" component={Signup} />
+          <Route path ="/Login" component={Login} />
           <Route component={DefaultPage} />
         </Switch>
       </Router>
