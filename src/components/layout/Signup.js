2a11228f3e021fe17cf5ce143aab2f5fa40437a7
Allan <kipropsallan@gmail.com>
2022-09-15 10:53:31 +0300

    Done

--- src/components/layout/Signup.js
+++ src/components/layout/Signup.js
@@ -1,34 +1,47 @@
 import React from 'react';
 
-function Signup() {
-    return(
-      <div className="form">
-          <div className="form-body">
-              <div className="username">
-                  <label className="form__label" for="firstName">First Name </label>
-                  <input className="form__input" type="text" id="firstName" placeholder="First Name"/>
-              </div>
-              <div className="lastname">
-                  <label className="form__label" for="lastName">Last Name </label>
-                  <input  type="text" name="" id="lastName"  className="form__input"placeholder="LastName"/>
-              </div>
-              <div className="email">
-                  <label className="form__label" for="email">Email </label>
-                  <input  type="email" id="email" className="form__input" placeholder="sang@xyz.com"/>
-              </div>
-              <div className="password">
-                  <label className="form__label" for="password">Password </label>
-                  <input className="form__input" type="password"  id="password" placeholder="Password"/>
-              </div>
-              <div className="confirm-password">
-                  <label className="form__label" for="confirmPassword">Confirm Password </label>
-                  <input className="form__input" type="password" id="confirmPassword" placeholder="Confirm Password"/>
-              </div>
-          </div>
-          <div class="footer">
-              <button type="submit" class="btn">Register</button>
-          </div>
-      </div>      
-    )       
-}
-export default Signup;
+function Signup(){
+    return (
+      <form>
+        <h3>Sign Up</h3>
+        <div className="mb-3">
+          <label>First name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Jane"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Last name</label>
+          <input type="text" className="form-control" placeholder="Doe" />
+        </div>
+        <div className="mb-3">
+          <label>Email</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="user@email.com"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="********"
+          />
+        </div>
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Sign Up
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          Already registered <a href="/login">Login?</a>
+        </p>
+      </form>
+    )
+  }
+
+  export default Signup
